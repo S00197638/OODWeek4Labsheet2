32d736c9b5c0e3bbe201c8d88d259098cb677db9
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2021-02-17 11:50:45 +0000

    OODWeek4Labsheet2 Exercise 4 Complete.

---
+++ Exercise4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+            //Query
+            var query = from number in numbers
+                        select DoubleIt(number);
+
+            //Lambda
+            //var query = numbers.Select(n => DoubleIt(n));
+
+            Console.WriteLine("Before the foreach loop");
+
+            foreach (var item in query)
+                Console.WriteLine(item);
+
+            Console.ReadLine();
+        }
+
+        private static int DoubleIt(int value)
+        {
+            Console.WriteLine("About to double the number " + value.ToString());
+            return value * 2;
+        }
+    }
+}
