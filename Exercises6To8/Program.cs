604c93d58be989c0d2a9d64df8debed199ed26cf
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2021-02-17 12:26:55 +0000

    OODWeek4Labsheet2 Exercise 6 Complete.

---
+++ Exercises6To8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercises6To8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John" };
+
+            #region Exercise6
+            //Query
+            #region Query
+            //var outputNames = from name in names
+            //                        orderby name ascending
+            //                        select name;
+            #endregion
+
+            //Lambda
+            #region Lambda
+            var outputNames = names.OrderBy(n => n);
+            #endregion
+
+            foreach (string name in outputNames)
+                Console.WriteLine(name);
+            #endregion
+        }
+    }
+}
