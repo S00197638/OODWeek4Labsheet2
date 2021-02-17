371b05894edd7934ed39835ff5e6e02104e7cfc0
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2021-02-17 11:02:39 +0000

    OODWeek4Labsheet2 Exercise 1 Complete.

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise1
 {
@@ -6,7 +7,35 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            
+            NumbersQuery();
+            //NumbersLambda();
+        }
+
+        private static void NumbersQuery()
+        {
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var outputNumbers = from number in numbers
+                                where number > 5
+                                orderby number
+                                select number;
+
+            foreach (int number in outputNumbers)
+                Console.WriteLine(number.ToString());
+
+            Console.ReadLine();
+        }
+
+        private static void NumbersLambda()
+        {
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var outputNumbers = numbers.Where(n => n > 5).OrderByDescending(n => n);
+
+            foreach (int number in outputNumbers)
+                Console.WriteLine(number.ToString());
+
+            Console.ReadLine();
         }
     }
 }
